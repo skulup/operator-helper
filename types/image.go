b4de3d9ffe3e541ead30ca90ca87f585a1bc251d
kaja <shawalpha5@gmail.com>
2020-10-17 03:36:19 +0000

    Added copyright headers

--- types/image.go
+++ types/image.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Skulup Ltd
+ * Copyright 2020 Skulup Ltd, Open Collaborators
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
